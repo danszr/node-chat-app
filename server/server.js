e7188ad48ba050aaba4d94c2643edcfb951e7c1f
Dan Selzer <dselzer@optonline.net>
2017-08-03 01:15:48 -0400

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,9 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log("New user connected");
 
+  socket.emit('newMessage', {
+    from: 'jenzhang',
+    text: 'Hey, what is going on',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (Message) => {
+    console.log('createMessage', Message);
+  });
+
   socket.on('disconnect', () => {
     console.log("User disconnected");
-  })
+  });
 });
 
 server.listen(port, () => {
