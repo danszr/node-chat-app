af8ac30974826e4c7c9be3c5d4980cd9149560b5
Dan Selzer <dselzer@optonline.net>
2017-08-07 15:42:24 -0400

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,15 @@ socket.on('newMessage', function (message) {
   $('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+  var li = $('<li></li>');
+  var a = $('<a target="_blank">My current location</a>')
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  $('#messages').append(li);
+});
 
 jQuery(document).ready(function () {
   jQuery('#message-form').on('submit', function (e) {
@@ -27,5 +36,22 @@ jQuery(document).ready(function () {
     }, function () {
 
     });
+  });
+});
+
+var locationButton = jQuery('#send-location');
+
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function (e) {
+    alert(e);
   });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -8,6 +8,19 @@ describe('generateMessage', () => {
 
     expect(res.from).toBe("Jen Zhang");
     expect(res.text).toBe("Hey");
+    expect(res.createdAt).toBeA("number");
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = "jen zhang";
+    var latitude = 45;
+    var longitude = 40;
+    var res = generateLocationMessage(from, latitude, longitude);
+
+    expect(res.from).toBe(from);
+    expect(res.url).toBe(`https://www.google.com/maps?q=45,40`);
     expect(res.createdAt).toBeA("number");
   });
 });
