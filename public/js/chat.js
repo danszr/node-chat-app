c2a8fd48b31438c20b852e3dc409e6e1011a3536
Dan Selzer <dselzer@optonline.net>
2017-08-08 18:12:54 -0400

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -75,7 +75,6 @@ jQuery(document).ready(function () {
     var messageTextbox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
-      from: 'User',
       text: messageTextbox.val()
     }, function () {
       messageTextbox.val('');
